da9c957c789676aad89045ec01b7e340fb971e7f
David Lieu <d.lieu@live.com>
2023-09-25 12:42:11 -0400

    - update Canvas test

--- client/src/components/Canvas/Canvas.test.tsx
+++ client/src/components/Canvas/Canvas.test.tsx
@@ -1,6 +1,5 @@
 import { render, fireEvent } from "@testing-library/react";
 import Canvas from "./Canvas";
-import { drawInitialCanvas } from "../../websocketHelpers/socketIO";
 
 // Mock the UserContext module to provide mock values
 jest.mock("../../hooks/UserContext", () => ({
@@ -28,7 +27,7 @@ describe("Canvas Component", () => {
     fireEvent.mouseMove(canvas);
     fireEvent.mouseUp(canvas);
 
-    // You can assert here that the canvas has been drawn as expected.
-    // You might need to use testing utilities like pixel color comparisons.
+    // We can assert here that the canvas has been drawn as expected.
+    // test pixel color comparisons.
   });
 });
